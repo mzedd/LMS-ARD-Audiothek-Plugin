df51be7c7d49e0c573a80fed6b74624b00267311
Max Zimmermann <49058787+mzedd@users.noreply.github.com>
2024-04-21 14:52:51 +0200

    Revert "Update API.pm - exclude sections without title (#28)"
    This reverts commit b20f6ec2f00a34f4b0d07f0d182c5ddfc69331cc.

--- ARDAudiothek/API.pm
+++ ARDAudiothek/API.pm
@@ -219,12 +219,7 @@ sub _sectionsToLists {
             );
             next;
         }
-        
-        # exclude sections without a title from our list
-        if($section->{title} eq "") {
-              next;
-        }
-        
+
         if($section->{type} eq "program_sets" or $section->{type} eq "featured_programset") {
             push (@items, {
                     title => $section->{title},
